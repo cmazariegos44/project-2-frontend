4d861d6ecdf95cd5f23945f7ab5ca624420a826d
cmazariegos44 <41842292+cmazariegos44@users.noreply.github.com>
2020-08-04 14:41:49 -0400

    front end changes

--- app.js
+++ app.js
@@ -1,53 +1,66 @@
 // ==NAV BAR ONLY==
 
-// let firstDiv = $(".navbar").append('<div class ="brand-title"> Claudia\'s Store</div>')
-// let firstAttr = $(".navbar").append('<a href ="#" class="toggle-button"><span class="bar"></span> <span class="bar"></span> <span class="bar"></span> </a>')
-// let secondDiv = $(".navbar").append('<div class="navbar-links"><ul><li><a href="#"><a class="aaa" href="#startabout">About Us</a></a></li><li><a href="#"><a class="aaa" href="#skills">Home</a></a></li><li><a href="#"><a class="aaa" href="#portfolio">Shop</a></li></ul></div>')
+let firstDiv = $(".navbar").append('<div class ="brand-title"> Claudia\'s Shop</div>')
+let firstAttr = $(".navbar").append('<a href ="#" class="toggle-button"><span class="bar"></span> <span class="bar"></span> <span class="bar"></span> </a>')
+let secondDiv = $(".navbar").append('<div class="navbar-links"><ul><li><a class="aaa" href="#startabout">About Us</a></li><li><a class="aaa" href="#skills">Shop</a></li><li><a class="aaa" href="#contact">Contact</a></li></ul></div>')
 
-// const toggleButton = document.getElementsByClassName('toggle-button')[0]
-// const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-// toggleButton.addEventListener('click', () => {
-//   navbarLinks.classList.toggle('active')
-// })
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
 // ==NAV BAR ONLY end ==
+
+
+//get data from BACKEND 
 
 const addItems = function(dataGet){
     //append something to root id
     for(var i = 0; i < dataGet.length; i++){
     $(".root").append(`<p class= "item">${dataGet[i].name}</p>`);
-    $(".item").append(`<img src="${dataGet[i].url}"/>`);
+    $(".item").append(`<img src="${dataGet[i].url}">`);
+    // return -1;
     }
     console.log(dataGet, "This worked!")
 }
+//pulling backend END
 
+
+
+
+//Attempt At AXIOS//
+// gets the initial data for images and name
 axios.get('http://localhost:3000/shop/poop').then(response => {
-    
-  // gets the initial data
   addItems(response.data)
   console.log(response, "hello")
-  console.log(response.data, "hi")
-  
+  // console.log(response.data, "hi")
 })
+//Attempt At AXIOS//
+
+
+//NEED CRUD
+
+//Alex's GIPHY CODE
+// const shops = document.querySelector('.shops')
+
+// function addPictures (shopData) {
+//   // Adds all of the gifs to the dom
+//   shops.innerHTML = ''
+//   shopData.forEach(shop => {
+//     if (!shop.url) return
+
+//     const imageNode = document.createElement('img')
+//     imageNode.setAttribute('src', shop.url)
+//     imageNode.classList.add('shop')
+
+//     imageNode.addEventListener('click', () => { editModal(shop) })
+
+//     shops.appendChild(imageNode)
+//   })
+// }
 
 
 
 
-//Alex's CLASS WORK
-// const $shopSelect = $("#createselect");
-// const $shopEditSelect = $("#editselect");
 
-// const getItems = async () => {
-//     //API CALL USING ASYNC/AWAIT
-//     const response = await fetch(`${URL}/shop`);
-//     const data = await response.json();
-//     //POPULATE SELECTOR WITH RETRIEVED DATA
-//     data.forEach((shop) => {
-//       const $option = $("<option>").attr("value", shop._id).text(shop.name);
-//       $shopSelect.append($option);
-//       const $option2 = $("<option>").attr("value", shop._id).text(shop.name);
-//       $shopEditSelect.append($option2);
-//     }); 
-//   };
-
-//   getItems()
